6f0b158f7bd57d382e218d22b4b31ee54ff7bf8d
huangguangda <2397923107@qq.com>
2018-01-26 19:25:56 +0800

    11111111111111ok

--- app/src/main/java/cn/edu/gdmec/android/androidlogcat/MainActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/androidlogcat/MainActivity.java
@@ -2,6 +2,7 @@ package cn.edu.gdmec.android.androidlogcat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate ( savedInstanceState );
         setContentView ( R.layout.activity_main );
+
+        Log.v ("MainActivity","Verbose");
+        Log.d ("MainActivity","Debug");
+        Log.i ( "MainActivity", "Info" );
+        Log.w ( "MainActivity", "Warning" );
+        Log.e ( "MainActivity", "Error" );
+
     }
 }
